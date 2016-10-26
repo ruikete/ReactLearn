2e5915f32ed9d282cc1206024a27c6747bce977f
Terry <blueterry@yahoo.com>
2016-10-26 22:55:19 +0800

    update copy

--- app/components/examples.jsx
+++ app/components/examples.jsx
@@ -7,6 +7,6 @@ var React = require('react');
 //         );
 //     }
 // });
-var Examples = (props) => (<h3>Examples Component</h3>);
+var Examples = (props) => (<h3>Examples Component!</h3>);
 
 module.exports = Examples;
